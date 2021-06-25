e242cbe09dc37dac1e922da2c13d8dc270a28818
Raluca Preda <raluca.preda@cdsa.nl>
2021-06-25 09:00:13 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Playwright;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static async Task Main()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser 
+            //= await playwright.Chromium.LaunchAsync();
+            = await playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+                SlowMo = 200,
+            });
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://playwright.dev/dotnet");
+        await page.ScreenshotAsync(new PageScreenshotOptions { Path = "screenshot.png" });
+    }
+}
