26904e5df876a099855d5f2123e27de69c8be4b3
Nicu Ioana <ioana.nicu@cegeka.com>
2021-06-25 12:21:42 +0300

    Test Emag

--- PlaywrightTests/UnitTest1.cs
+++ PlaywrightTests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Microsoft.Playwright.NUnit;
 using NUnit.Framework;
+using Core;
 
 namespace PlaywrightTests
 {
@@ -18,8 +19,19 @@ namespace PlaywrightTests
         [Test]
         public async Task ShouldMultiply()
         {
-            int result = await Page.EvaluateAsync<int>("() => 7 * 3");
-            Assert.AreEqual(21, result);
+            //await Core.Demo.TestEmag();
+            var page = Demo.GoTo("https://www.emag.ro/").Result;
+
+            // Click text=PC, Periferice & Software
+            page = await Demo.ClickText(page, "PC, Periferice & Software");
+
+            // Click text=TV, Audio-Video & Foto
+            page = await Demo.ClickText(page, "TV, Audio-Video & Foto");
+
+            // Click text=4K Ultra HD
+            page = await Demo.ClickText(page, "4K Ultra HD");
+            Assert.AreEqual("https://www.emag.ro/televizoare/filter/claritate-imagine-f9180,4k-v-8182405/c?ref=hp_menu_quick-nav_190_3&type=filter", page.Url);
+
         }
     }
 }
